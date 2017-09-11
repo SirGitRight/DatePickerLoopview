ef18d883275baf60f49148efeea5bb781d4fea16
dgf <157636336@qq.com>
2017-09-11 17:25:45 +0800

    first commit 2

---
+++ testlibrary/src/main/java/com/example/testlibrary/testLibrary.java
@@ -0,0 +1,18 @@
+package com.example.testlibrary;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2017/9/11/011.
+ */
+
+public class testLibrary  extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_library);
+    }
+}
